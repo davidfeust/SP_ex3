bf99b8cb312e9d1e745abb3ddb8ae3d4ea137151
davidfeust <david.feust@gmail.com>
2020-12-23 17:08:59 +0200

    sort

--- isort.c
+++ isort.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+#define SIZE 50
+
+void shift_element(int *arr, int i) {
+    for (int j = i; j > 0; j--) {
+        *(arr + j) = *(arr + j - 1);
+    }
+}
+
+void insertion_sort(int *arr, int len) {
+    for (int i = 0; i < len; ++i) {
+        int temp = *(arr + i);
+        for (int j = 0; j < i; ++j) {
+            if (*(arr + j) > temp) {
+                shift_element((arr + j), i - j);
+                *(arr + j) = temp;
+                break;
+            }
+        }
+    }
+}
+
 int main() {
-    printf("Sort!\n");
+    int arr[SIZE];
+    for (int i = 0; i < SIZE; ++i) {
+        scanf("%d", arr + i);
+    }
+    insertion_sort(arr, SIZE);
+    for (int i = 0; i < SIZE; ++i) {
+        printf("%d,", *(arr + i));
+    }
     return 0;
 }
